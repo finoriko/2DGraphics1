d059866fc6221f4ba589f3599d02e28a1d78f382
finoriko <finoriko@Hotmail.com>
2020-06-29 00:11:17 +0900

    ModuleProject add

---
+++ Modules/WindowCreator.cpp
@@ -0,0 +1,1 @@
+#include "../include/WindowCreator.h"
---
+++ include/WindowCreator.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef INCLUDED_GAMELIB_WINDOWCREATOR_H
+#define INCLUDED_GAMELIB_WINDOWCREATOR_H
+
+namespace GameLib
+{
+	class WIndowCreator
+	{
+	public:
+		class Configuration
+		{
+		public:
+			void setWidth(int width);
+			void setHeight(int height);
+		};
+	};
+}
+
+#endif
