9b824270080bb67d936aaf6d779b2557b9ca71d9
finoriko <finoriko@Hotmail.com>
2020-07-03 01:07:02 +0900

    GameLib Modify

---
+++ GameLib/Framework.cpp
@@ -0,0 +1,23 @@
+#include "Framework.h"
+#include "FontTextureGenerated.h"
+
+#include <string>
+
+using namespace std;
+
+namespace GameLib
+{
+	namespace
+	{
+		class Impl
+		{
+		public:
+
+		};
+		Impl* gImpl = 0;
+	}
+	Framework::Framework()
+	{
+	}
+	
+}
---
+++ GameLib/Framework.h
@@ -0,0 +1,41 @@
+#ifndef INCLUDED_GAMELIB_FRAMEWORK_H
+#define INCLUDED_GAMELIB_FRAMEWORK_H
+namespace GameLib {
+	class Framework
+	{
+	public:
+		Framework();
+		//사용자 정의 함수.이것을 쓴다.
+		void update();
+		///인스턴스 받기
+		static Framework instance();
+		///화면폭 가져오기
+		int width() const;
+		///와면 높이 가져오기
+		int height() const;
+		///VRAM 받아오기
+		unsigned* videoMemory();
+		///Framework종료명령 내리기
+		void requestEnd();
+		///Framework에 종료명령이 내려저 있는지 알아보기
+		bool isEndRequested() const;
+
+		//아래 라이브러리 사용자는 몰라도 된다
+		void start(void* windowHandle);
+		void preUpdate();
+		void postUpdate();
+		static void create();
+		static void destroy();
+	};
+	class StandardInput {
+	public:
+		StandardInput();
+		StandardInput& operator>>(char&);
+	private:
+		bool mPrevEnterOn;
+	};
+	extern StandardInput cin;
+
+} //namespace GameLib
+
+#endif
