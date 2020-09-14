9a8b3ac7486d89dcc933f1b9d023587163b43c8c
finoriko <finoriko@Hotmail.com>
2020-09-15 00:16:41 +0900

    Vector and VectorImpl add

---
+++ include/GameLib/Base/Impl/VectorImpl.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_GAMELIB_VECTORIMPL_H
+#define INCLUDED_GAMELIB_VECTORIMPL_H
+
+#include "GameLib/GameLib.h"
+#include "GameLib/Base/Vector.h"
+
+namespace GameLib {
+
+
+} //namespace GameLib
+
+#endif
---
+++ include/GameLib/Base/Vector.h
@@ -0,0 +1,12 @@
+#ifndef INCLUDED_GAMELIB_VECTOR_H
+#define INCLUDED_GAMELIB_VECTOR_H
+
+namespace GameLib {
+
+
+
+} //namespace GameLib
+#include "GameLib/Base/Impl/VectorImpl.h" //ριγσͺΟͺ³ͺΞρι
+
+#endif
+
